53de46df8295d8b32dee50b59033a5c630add847
Alex Schenkel <alex@Alexs-MacBook-Pro-8.local>
2023-04-04 16:43:46 +1200

    anodda one +1

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ function doQueryCount() {
 }
 
 // Have Node serve the files for our built React app
-// app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // app.use(bodyParser.json());
 app.use(express.raw());
