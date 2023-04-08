9b7d016f71436355311ef917fa45baa40dd0dc17
Alex Schenkel <alex@Alexs-MacBook-Pro-8.local>
2023-04-08 23:00:11 +1200

    testing stuff

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ app.use(express.json({limit: '50mb'}));
 // app.use(express.urlencoded({limit: '50mb'}));
 // app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(cors({ origin: false}));
+app.use(cors({ origin: '*'}));
 
 
 
