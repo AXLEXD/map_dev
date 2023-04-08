7728132f2b1feac06de19904ba97ccdc3ceca763
Alex Schenkel <alex@Alexs-MacBook-Pro-8.local>
2023-04-09 00:39:26 +1200

    Set pixel-canvas.github.io as allowed origin

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.use(express.json({limit: '50mb'}));
 // app.use(express.urlencoded({limit: '50mb'}));
 // app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(cors({ origin: '*'}));
+app.use(cors({ origin: 'https://pixel-canvas.github.io'}));
 
 
 
